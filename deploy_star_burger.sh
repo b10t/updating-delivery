95bcb39758a08a9c30523eee2d962b4661770142
b10t <b10t.rus@gmail.com>
2022-09-16 17:01:28 +0700

    Edit deploy script.

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 set -e
 
+cd /opt/updating-delivery
+
+source env/bin/activate
+
 git pull
+
+pip install -r requirements.txt
+npm ci --dev
+
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
+
+./manage.py collectstatic --noinput
+./manage.py migrate
+
+systemctl reload nginx.service
+systemctl restart start-burger.service
+
+deactivate
+
+echo "Проект успешно обновлён."
